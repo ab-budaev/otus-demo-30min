6c00d8ae35423d43711d61f02f17873cc0b09f5c
Aleksei Budaev <ab.budaev@alyce.com>
2021-03-22 19:30:49 +0300

    Added Tideways xhprof as PHP extension

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
+/** @noinspection PhpUndefinedConstantInspection */
+/** @noinspection PhpUndefinedFunctionInspection */
+
+tideways_xhprof_enable(TIDEWAYS_XHPROF_FLAGS_MEMORY | TIDEWAYS_XHPROF_FLAGS_CPU | TIDEWAYS_XHPROF_FLAGS_NO_BUILTINS);
 
 echo "test";
+
+file_put_contents(
+    sys_get_temp_dir() . DIRECTORY_SEPARATOR . uniqid('', true) . '.myapplication.xhprof',
+    serialize(tideways_xhprof_disable())
+);
